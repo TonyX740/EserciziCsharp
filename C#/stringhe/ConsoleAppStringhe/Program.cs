dde667195641d830f1f35a731518213d29689a76
TonyX740 <107193788+TonyX740@users.noreply.github.com>
2025-07-30 17:33:58 +0200

    Upload ultima parte esercizi
    Caricamento ultima parte degli esercizi svolti finora

---
+++ C#/stringhe/ConsoleAppStringhe/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ConsoleAppStringhe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //creazione stringa
+            string stringa = "prova";
+
+            //stampo il contenuto della stringa
+            Console.WriteLine(stringa);
+
+            //esempio di escape, \n per andare da capo e \" per far stampare il carattere speciale "
+            string citazione = "citazione: \n\"Si vive una volta sola\"";
+
+            /*
+            la stringa che verrà stampata è
+            citazione: 
+            "Si vive una volta sola"
+            */
+            Console.WriteLine(citazione);
+
+            string nome = "Antonio";
+            string cognome = "Ciccarelli";
+
+            //esempio di concatenazione, si usa il + per concatenare più stringhe
+            string presentazione = "ciao sono " + nome + " " + cognome;
+
+            //la stringa che verrà stampata sarà: ciao sono Antonio Ciccarelli
+            Console.WriteLine(presentazione);
+
+            //esempio di formattazione, usiamo {n} per stampare le variabili
+            //senza chiudere la stringa
+            //la stringa che verrà stampata sarà: salve sono Antonio Ciccarelli
+            Console.WriteLine("salve sono {0} {1}", nome, cognome);
+
+            //è inoltre possibile usare la formattazione su una stringa tramite string.Format
+            string presentazione2 = string.Format("salve sono {0} {1}", nome, cognome);
+
+            //esempio di interpolazione, scriviamo $ prima della stringa e dentro la stringa
+            //mettiamo tra parentesi graffe i nomi delle variabili da stampare
+            //la stringa che verrà stampata sarà: salve sono Antonio Ciccarelli
+            Console.WriteLine($"buongiorno sono {nome} {cognome}");
+
+            //esempio metodo toUpper(),tutti i caratteri di una stringa diventano maiuscoli
+            //la stringa che verrà stampata sarà: SALVE SONO ANTONIO CICCARELLI
+            Console.WriteLine(presentazione2.ToUpper());
+
+            //esempio metodo toLower(),tutti i caratteri di una stringa diventano minuscoli
+            //la stringa che verrà stampata sarà: salve sono antonio ciccarelli
+            Console.WriteLine(presentazione2.ToLower());
+
+            //esempio metodo Lenght, restituisce un numero intero che equivale
+            //alla lunghezza della stringa
+            //il numero stampato sarà 29
+            Console.WriteLine(presentazione2.Length);
+
+            //esempio di indexing, restituisce la lettera presente nella posizione indicata
+            //il carattere restituito sarà s
+            Console.WriteLine(presentazione2[0]);
+
+            //esempio metodo IndexOf(), restituisce un numero intero che equivale a
+            //la posizione del carattere indicato, in caso di più caratteri uguali 
+            //restituisce la posizione del primo carattere trovato, se il carattere
+            //non viene trovato restituisce -1
+            //il numero stampato sarà 1
+            Console.WriteLine(presentazione2.IndexOf('a'));
+
+            //esempio metodo Substring(), restituisce una stringa partendo dalla
+            //posizione indicata(si consiglia fortemente di guardare la documentazione)
+            //la stringa che verrà stampata sarà: Antonio Ciccarelli
+            Console.WriteLine(presentazione2.Substring(11));
+        }
+    }
+}
---
+++ C#/trycatch/ConsoleAppTryCatch/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace ConsoleAppTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esempio di calcolatrice, prende in input due numeri e stampa
+            //la somma di questi ultimi, da errore se non diamo un input o non
+            //mettiamo un numero, gli errori vengono gestiti dai try catch,
+            //questo ci permette di gestire le varie eccezioni e di non far
+            //crashare il programma e quindi terminarlo normalmente
+            int num1 = 0;
+            int num2 = 0;
+
+            //dentro il try mettiamo il codice che potrebbe lanciare eccezioni
+            try
+            {
+                Console.WriteLine("Sommiamo due numeri \nInserisci il primo:");
+                //in questo caso int.Parse() può lanciare diverse eccezioni
+                //che sono ArgumentNullException, FormatException e OverflowException
+                num1 = int.Parse(Console.ReadLine());
+            }
+            //in caso di eccezione entra in gioco il catch
+            //questo catch entra in gioco se non inseriamo nulla
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Errore: numero non inserito");
+            }
+            //questo catch entra in gioco se inseriamo uno o più caratteri
+            //al posto di un numero
+            catch (FormatException)
+            {
+                Console.WriteLine("Errore: carattere inserito al posto del numero");
+            }
+            //questo catch entra in gioco se inseriamo un numero troppo grande,
+            //per esempio mettiamo un numero che non entra in una variabile intera
+            catch (OverflowException)
+            {
+                Console.WriteLine("Errore: numero troppo grande");
+            }
+            //quello scritto sopra vale anche per i try catch qui sotto
+            try
+            {
+                Console.WriteLine("Inserisci il secondo:");
+                num2 = int.Parse(Console.ReadLine());
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Errore: numero non inserito");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Errore: carattere inserito al posto del numero");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Errore: numero troppo grande");
+            }
+            //il codice dentro finally viene eseguito sia in caso di successo che di eccezione
+            //questo può essere utile per esempio per chiudere l'accesso ad un file o ad un DB
+            finally
+            {
+                Console.WriteLine($"La somma dei numeri {num1} e {num2} è: {num1 + num2}");
+            }
+
+            Console.WriteLine("Ciao ciao");
+        }
+    }
+}
