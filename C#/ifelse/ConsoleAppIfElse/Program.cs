9ee1401be525b11ca690e220db0b4d2a3c160202
TonyX740 <107193788+TonyX740@users.noreply.github.com>
2025-07-31 16:45:30 +0200

    Aggiunta commenti ad esercizio ifelse

--- C#/ifelse/ConsoleAppIfElse/Program.cs
+++ C#/ifelse/ConsoleAppIfElse/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleAppIfElse
     {
         static void Main(string[] args)
         {
+            //esempio if else
+            //programma che usa le condizioni if else per calcolare l'anno bisestile
             int anno = 0;
             bool isCorrect = true;
             Console.WriteLine("Ciao, questo programma calcola se un anno è bisestile o no");
@@ -29,52 +31,69 @@ namespace ConsoleAppIfElse
                 Console.WriteLine("Il numero è troppo grande");
                 isCorrect = false;
             }
-            if (isCorrect)
+            if (isCorrect)  //equivale a scrivere if (isCorrect == true)
             {
+                //se ifcorrect è uguale a true questa parte di codice viene eseguita
                 if (anno > 1581)
                 {
+                    //se l'anno è maggiore di 1581 questa parte di codice viene eseguita
                     if (anno % 4 == 0)
                     {
+                        //se l'anno diviso 4 da resto 0 questa parte di codice viene eseguita
                         if (anno % 100 != 0)
                         {
+                            //se l'anno diviso 100 non da resto 0 questa parte di codice viene eseguita
                             Console.WriteLine($"{anno} è un anno bisestile");
                         }
                         else if (anno % 100 == 0 && anno % 400 == 0)
                         {
+                            // altrimenti se l'anno diviso 100 da resto 0 e anche 
+                            // diviso 400 da resto 0 questa parte di codice viene eseguita
                             Console.WriteLine($"{anno} è un anno bisestile");
                         }
                         else
                         {
+                            //se nessuna delle condizioni sopra viene rispettata
+                            //questa parte di codice viene eseguita
                             Console.WriteLine($"{anno} non è un anno bisestile");
                         }
                     }
                     else
                     {
+                        //se l'anno diviso 4 non da resto 0 questa parte di codice viene eseguita
                         Console.WriteLine($"{anno} non è un anno bisestile");
                     }
                 }
                 else if (anno > 3 && anno <= 1581)
                 {
+                    // altrimenti se l'anno è maggiore di 3 e anche 
+                    // minore o uguale a 1581 questa parte di codice viene eseguita
                     if (anno % 4 == 0)
                     {
+                        //se l'anno diviso 4 da resto 0 questa parte di codice viene eseguita
                         Console.WriteLine($"{anno} è un anno bisestile");
                     }
                     else
-                    {
+                    {   //altrimenti viene eseguita quest'altra
                         Console.WriteLine($"{anno} non è un anno bisestile");
                     }
                 }
                 else if (anno > 0 && anno <= 3)
                 {
+                    // altrimenti se l'anno è maggiore di 0 e anche 
+                    // minore o uguale a 3 questa parte di codice viene eseguita
                     Console.WriteLine($"{anno} non è un anno bisestile");
                 }
                 else if (anno <= 0)
                 {
+                    // altrimenti se l'anno è 
+                    // minore o uguale a 0 questa parte di codice viene eseguita
                     Console.WriteLine("Anno non valido");
                 }
             }
             else
             {
+                //se ifcorrect è uguale a false questa parte di codice viene eseguita
                 Console.WriteLine("Input non valido");
             }
         }
