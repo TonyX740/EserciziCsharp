6396cafd22247b90a8d202d8a601c79ceafefd56
TonyX740 <107193788+TonyX740@users.noreply.github.com>
2025-08-17 17:56:20 +0200

    Caricamento esercizi switch e while

---
+++ C#/switch/ConsoleAppSwitch/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace ConsoleAppSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esempio di costrutto switch che stampa cattiverie gratuite in base all'età inserita
+            int eta = 0;
+            bool isValid = true;
+            Console.WriteLine("Benvenuto, inserisci la tua età");
+            try
+            {
+                eta = int.Parse(Console.ReadLine());
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("ArgumentNullException");
+                isValid = false;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("FormatException");
+                isValid = false;
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("OverflowException");
+                isValid = false;
+            }
+            if (isValid)
+            {
+                Console.WriteLine($"Hai {eta} anni");
+                //creazione operatore switch switch che prende in considerazione eta
+                switch (eta)
+                {
+                    case 0:
+                        //se eta = 0 esegui queste istruzioni
+                        Console.WriteLine("Benvenuto al mondo (chi te lo ha fatto fare?)");
+                        break;  //termina lo switch e vai avanti col programma
+                    case 18:
+                        //se eta = 18 esegui queste istruzioni
+                        Console.WriteLine("Sei diventato maggiorenne yeeeeee (mi dispiace)");
+                        break;  //termina lo switch e vai avanti col programma
+                    case 30:
+                        //e così via, non ho voglia di scriverlo per tutti
+                        Console.WriteLine("Complimenti per il traguardo (credo...)");
+                        break;  //tanto si capisce
+                    case 40:
+                        Console.WriteLine("Passa in fretta il tempo (quanti capelli ti sono rimasti?)");
+                        break;
+                    case 50:
+                        Console.WriteLine("Mezzo secolo che sei in vita, complimenti (hai ancora i capelli?\nSe si di che colore?)");
+                        break;
+                    case 60:
+                        Console.WriteLine("Daje tutta, ancora in carreggiata (non investire nessuno però)");
+                        break;
+                    case 70:
+                        Console.WriteLine("Ed eccoti qui (perché qui e non facebook?)");
+                        break;
+                    case 80:
+                        Console.WriteLine("Bene, esisti da quasi un secolo (non dimenticare le medicine se vuoi superare il record)");
+                        break;
+                    case 90:
+                        Console.WriteLine("Azz, stai ancora qua? (Ma complimenti, mica siamo 8 miliardi di noi, nono)");
+                        break;
+                    case 100:
+                        Console.WriteLine("Wow ma sei ancora in vita? Complimenti");
+                        break;
+                }
+                Console.WriteLine("Ok switch finito, vai via adesso");
+            }
+            else
+            {
+                Console.WriteLine("Età non valida");
+            }
+        }
+    }
+}
---
+++ C#/while/ConsoleAppWhile/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace ConsoleAppWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esempio cicli while e do while, cicli che vengono eseguiti finché 
+            //le condizioni vengono rispettate
+            int condizione = 5;
+
+            Console.WriteLine("Esempio ciclo while");
+            //controllo in testa, il codice dentro il ciclo while viene eseguito solo e finché la condizione viene rispettata
+            //questo succede perché la condizione viene controllata subito
+            while (condizione > 0)  //finché condizione maggiore di 0 il codice viene ripetuto
+            {
+                Console.WriteLine("ora la condizione viene rispettata e quindi mi vedi");
+                if (condizione > 2)
+                {
+                    Console.WriteLine($"mi rivedrai altre {condizione - 1} volte");
+                }
+                else if (condizione > 1)
+                {
+                    Console.WriteLine($"mi rivedrai un'ultima volta");
+                }
+                else
+                {
+                    Console.WriteLine("questa è l'ultima volta, ciao ciao");
+                }
+                condizione--;
+            }
+
+            Console.WriteLine("Esempio ciclo do while");
+            //controllo in coda, il codice dentro il do while viene sempre eseguito almeno una volta
+            //questo succede perché la condizione viene controllata alla fine
+            do
+            {
+                Console.WriteLine("ciao, adesso mi vedi anche se la condizione non viene rispettata");
+                Console.WriteLine($"adesso condizione vale {condizione}, ma dovrebbe essere maggiore di 5");
+                Console.WriteLine("ti saluto, ciao ciao");
+            } while (condizione > 5);   //finché condizione maggiore di 5 il codice viene ripetuto, 
+                                        //cosa che non succederà
+        }
+    }
+}
