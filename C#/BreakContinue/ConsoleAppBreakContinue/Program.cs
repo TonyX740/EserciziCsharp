cd671ce55361dd66e04fa24a40d35554dfcda00e
TonyX740 <107193788+TonyX740@users.noreply.github.com>
2025-08-17 17:58:47 +0200

    caricamento esercizi for e BreakContinue

---
+++ C#/BreakContinue/ConsoleAppBreakContinue/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace ConsoleAppBreakContinue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esempio di break
+            //break fa finire il ciclo, indipendentemente dalla sua condizione e dal tipo di ciclo (for, while ecc)
+            //in questo caso il ciclo finisce anche se i non ha raggiunto 10 e quindi rispetta la condizione
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 5)
+                {
+                    break;
+                }
+                Console.WriteLine($"i={i}");
+            }
+
+            //esempio di continue
+            //continue fa saltare una iterazione e continua con quella successiva
+            //nel nostro caso saltiamo le iterazioni se i è uguale ad un numero non divisibile per 2
+            for (int i = 0; i < 21; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ C#/for/ConsoleAppFor/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleAppFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //esempio di ciclo for
+            //il ciclo for è composto da iteratore (i, che viene visto solo dentro il for), condizione e incremento o decremento
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Ciao, sono un ciclo for con i={i}");
+            }
+
+            //esempio foreach
+            string[] nomi = ["tony", "vix", "neon", "Sashi", "Stronkler", "Elly", "basta"]; //lista di nomi (poi vediamo cos'è)
+
+            //il ciclo foreach viene usato per scorrere gli elementi dentro liste ed array
+            //in questo caso stiamo dicendo di eseguire un istruzione per ogni nome presente nella lista nomi
+            foreach (string nome in nomi)
+            {
+                Console.WriteLine(nome);
+            }
+
+        }
+    }
+}
